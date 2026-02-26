9aa80eee32929cac7185aac98075408c7587fe2c
k_shibuya <tikuwap@gmail.com>
2026-02-26 13:59:10 +0900

    feat: Refine design with modern glassmorphism and improve PWA settings

--- vite.config.ts
+++ vite.config.ts
@@ -8,24 +8,64 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'icon-512.png'],
+      includeAssets: ['favicon.ico', 'icon-512.png', 'icon-192.png'],
       manifest: {
-        name: 'ポケモンクイズ',
+        name: 'ポケモンクイズ - Pokemon Quiz',
         short_name: 'ポケクイズ',
-        description: '最強のポケモンマスターを目指せ！',
-        theme_color: '#ff80bf',
-        background_color: '#ffffff',
+        description: '最強のポケモンマスターを目指せ！クイズに答えてポケモンを集めよう！',
+        theme_color: '#667eea',
+        background_color: '#f0f4f8',
         display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        scope: '/',
         icons: [
           {
             src: 'icon-192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
             src: 'icon-512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ],
+        categories: ['games', 'education', 'entertainment'],
+        screenshots: []
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/pokeapi\.co\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'pokeapi-cache',
+              expiration: {
+                maxEntries: 500,
+                maxAgeSeconds: 60 * 60 * 24 * 7 // 7 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          },
+          {
+            urlPattern: /^https:\/\/raw\.githubusercontent\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'pokemon-images-cache',
+              expiration: {
+                maxEntries: 1000,
+                maxAgeSeconds: 60 * 60 * 24 * 30 // 30 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
           }
         ]
       }
